c896dd760e257b23a641462490d67d89cdf65bf1
Henry Schreiner <henryschreineriii@gmail.com>
2021-01-21 18:10:08 -0500

    fix: setup.cfg, supports adding submodules

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import sys
 
 try:
     from skbuild import setup
+    from setuptools import find_packages
 except ImportError:
     print(
         "Please update pip, you need pip 10 or greater,\n"
@@ -18,13 +19,8 @@ from setuptools import find_packages
 
 
 setup(
-    name="scikit_build_example",
-    version="0.0.1",
-    description="a minimal example package (with pybind11)",
-    author="Henry Schreiner",
-    license="MIT",
     packages=find_packages(where = 'src'),
-    package_dir={"": "src"},
-    cmake_install_dir="src/scikit_build_example",
+    package_dir={'': 'src'},
+    cmake_install_dir='src/scikit_build_example',
     include_package_data = True,
 )
