a11786bab058fe5db97ceee4762178ad0d6003ae
Haohan Yang <37651510+haohanyang@users.noreply.github.com>
2024-11-17 01:54:37 +0100

    Add type stub (#155)

---
+++ src/scikit_build_example/__init__.pyi
@@ -0,0 +1,19 @@
+"""
+Pybind11 example plugin
+-----------------------
+
+.. currentmodule:: scikit_build_example
+
+.. autosummary::
+    :toctree: _generate
+
+    add
+    subtract
+"""
+
+def add(i: int, j: int) -> int:
+    """
+    Add two numbers
+
+    Some other explanation about the add function.
+    """
